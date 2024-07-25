7061a4ce4e1776937d48f75f5c8a73f0e222fb34
TatsHats <6642017@gmail.com>
2024-07-25 17:09:47 +0300

    feat: modified package-lock.json and src/index.js files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
-  
+    const units_and_teens = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", 
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" 
+    ];
+    const tens = [    
+        "", "",  // для учета случаев когда десятки = 0 (не прописывается) или 1 (учтено как 10)
+        "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+    ];
+
+    if (number < 20) {
+        return units_and_teens[number];
+    }
+
+    if (number < 100) {
+        const tensNum = Math.floor(number / 10);
+        const unitsNum = number % 10;
+        return tens[tensNum] + (unitsNum ? ' ' + units_and_teens[unitsNum] : '');
+    }
+
+    if (number < 1000) {
+        const hundredsNum = Math.floor(number / 100);
+        const remainder = number % 100;
+        return units_and_teens[hundredsNum] + ' hundred' + (remainder ? ' ' + toReadable(remainder) : '');
+    }
+    return '';
 }
